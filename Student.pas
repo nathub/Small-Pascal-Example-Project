6d64fc49b4b29f287e23047f8a61ec611045b958
Nathan H <nathan.zan6037@gmail.com>
2025-10-19 16:31:50 -0300

    some progress on student.

--- Student.pas
+++ Student.pas
@@ -6,24 +6,33 @@ uses Grading, sysutils;
 
 type
     TNameRecord = record
-        firstname, middlename, lastname: string[30];
+        firstname, middlename, lastname: string[100];
     end;
 
     TGradeLevel = 0..12;
+    TGender = (male, female, other);
 
     TStudent = record
         name: TNameRecord;
-        gender: (male, female, other);
+        gender: TGender;
         dateofbirth: TDateTime;
         score: TGradeScore;
         grade: TGradeLetter;
         gradeLevel: TGradeLevel         //0 is kindergarden
     end;
 
+function CreateNameRecord(first, middle, last: string[100]): TNameRecord;
+function ParseGender(genderString: string[8]): TGender;
 function GradeLevelToString(level: TGradeLevel): string;
 function CreateStudent(): TStudent;
 
 implementation
+
+function CreateNameRecord(first, middle, last: string[100]): TNameRecord;
+begin
+
+
+end;
 
 function GradeLevelToString(level: TGradeLevel): string;
 begin
@@ -31,6 +40,27 @@ begin
         GradeLevelToString := 'Kindergarten'
     else
         GradeLevelToString := 'Grade ' + IntToStr(level);
+end;
+
+function CreateStudent( name: TNameRecord; 
+                        gender: TGender; 
+                        dob: TDateTime;
+                        score: TGradeScore;
+                        gradeLevel: TGradeLevel
+                        ): TStudent;
+begin
+    var returnStudent: TStudent;
+    returnStudent.name.firstname := name.firstname;
+    returnStudent.name.middlename := name.middlename;
+    returnStudent.name.lastname := name.lastname;
+
+    returnStudent.gender := gender;
+    returnStudent.dateofbirth := dob;
+    returnStudent.score := score;
+    returnStudent.grade := Grading.GetLetterFromScore(score);
+    returnStudent.gradeLevel := gradeLevel;
+
+    CreateStudent := returnStudent;
 end;
 
 end.
